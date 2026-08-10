824a076c962485c147de522ee26bc35088d4001f
Yuxuan Wang <111606884+w-yuxuan@users.noreply.github.com>
2026-08-10 18:44:03 -0400

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+        def dfs(n):
+            if n in visit:
+                return False
+                
+            visit.add(n)
+            thou = (n//1000)%10
+            hund  = (n//100)%10
+            ten =  (n//10)%10
+            one = n %10
+            
+            res = sum(int(d)**2 for d in str(n))
+            if res ==1:
+                return True
+            return dfs(res)
+
+        return dfs(n)
