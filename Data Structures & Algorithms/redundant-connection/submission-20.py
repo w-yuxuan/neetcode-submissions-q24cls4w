2d94e41aa8b6ac3ac72c99d04ff1d87faf4d9912
Yuxuan Wang <111606884+w-yuxuan@users.noreply.github.com>
2026-08-31 18:31:51 -0400

    Add: redundant-connection - submission-20

---
+++ Data Structures & Algorithms/redundant-connection/submission-20.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        mem = defaultdict(list)
+        
+        def find(n1,n2,visit):
+            if n1==n2:
+                return True
+            # if 
+            visit.add(n1)
+            for c in mem[n1]:
+                if c not in visit and find(c,n2,visit):
+                    return True
+
+        for n1, n2 in edges:
+            if find(n1,n2,set()):
+                return[n1,n2]
+            mem[n1].append(n2)
+            mem[n2].append(n1)
+                
